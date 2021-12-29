536fdf4d84579944ae63e5b4c63005e337a6cd45
reiluk <lreichert200@gmail.com>
2021-12-29 14:21:37 +0100

    changed from ' to "

--- YoutubeDLGUI.py
+++ YoutubeDLGUI.py
@@ -1,4 +1,3 @@
-# I'm a beginner. Please don't hate me
 from tkinter import ttk
 from tkinter import filedialog
 import youtube_dl
@@ -25,8 +24,8 @@ def threadmanager():
 
 def my_hook(d):
     global progress
-    if d['status'] == 'downloading':
-        progress = "Fortschritt:" + d['_percent_str'] + " Verbleibende Zeit: " + d['_eta_str'] + "  " + d["_speed_str"]
+    if d["status"] == "downloading":
+        progress = "Fortschritt:" + d["_percent_str"] + " Verbleibende Zeit: " + d["_eta_str"] + "  " + d["_speed_str"]
     elif d["status"] == "finished":
         progress = "Finished downloading in " + d["_elapsed_str"]
     msg.config(text=progress)
@@ -38,11 +37,11 @@ def started():
     error.pack()
     try:
         if selected.get() == 1:
-            ydl_options = {'format': 'bestaudio', 'noplaylist': 'True',
-                           'outtmpl': str(filename) + "/%(title)s" + ".%(ext)s",
+            ydl_options = {"format": "bestaudio", "noplaylist": "True",
+                           "outtmpl": str(filename) + "/%(title)s" + ".%(ext)s",
                            "ffmpeg_location": "C:/ffmpeg",
-                           'progress_hooks': [my_hook],
-                           'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}]}
+                           "progress_hooks": [my_hook],
+                           "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}]}
             if input.get().count(".com") == 1:
                 with youtube_dl.YoutubeDL(ydl_options) as ydl:
                     ydl.download([input.get()])
@@ -50,7 +49,7 @@ def started():
                 with youtube_dl.YoutubeDL(ydl_options) as ydl:
                     ydl.download([f"ytsearch:{input.get()}"])
         elif selected.get() == 2:
-            ydl_options = {'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]', 'noplaylist': 'True',
+            ydl_options = {"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]", "noplaylist": "True",
                            "outtmpl": str(filename) + "/%(title)s" + ".%(ext)s",
                            "progress_hooks": [my_hook],
                            "ffmpeg_location": "C:/ffmpeg"}
@@ -74,10 +73,6 @@ root.title("Youtube-DL")
 root.geometry("300x220")
 root.minsize(300, 220)
 root.maxsize(300, 220)
-try:
-    root.iconphoto(False, tk.PhotoImage(file='C:/Users/Skryptex/Documents/youtube-dl gui/image_mini.png'))
-except tk.TclError:
-    pass
 title = ttk.Label(root, text="Youtube-DL GUI")
 title.pack()
 title = ttk.Label(root, text="Title or URL:")
@@ -85,8 +80,8 @@ title.pack()
 input = ttk.Entry(root, width=45)
 input.pack()
 selected = tk.IntVar()
-MP3 = ttk.Radiobutton(root, text='MP3', value=1, variable=selected)
-MP4 = ttk.Radiobutton(root, text='MP4', value=2, variable=selected)
+MP3 = ttk.Radiobutton(root, text="MP3", value=1, variable=selected)
+MP4 = ttk.Radiobutton(root, text="MP4", value=2, variable=selected)
 MP3.pack()
 MP4.pack()
 folder_path = tk.StringVar()
